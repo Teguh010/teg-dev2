935c0dd7a6447383fcda74c37bec5ee28db5f2a2
Teguh <teguhbadrusalam6@gmail.com>
2025-10-16 10:39:07 +0700

    updaet

--- pm2.config.js
+++ pm2.config.js
@@ -3,12 +3,16 @@ module.exports = {
     {
       name: 'TraceGrid frontend',
       script: './node_modules/next/dist/bin/next',
-      args: 'start',
+      args: 'start -p 3000 -H 0.0.0.0',
       exec_mode: 'cluster',
       watch: false,
       autorestart: true,
       instances: 'max',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        HOSTNAME: '0.0.0.0',
+      },
     },
   ],
 };
-
